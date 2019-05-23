493ec44670813b57d6972d758a41d6456f86986f
Shuhei Akutagawa <readonly@akutagawa.dev>
2019-05-23 16:17:32 +0900

    fish: Update Bobthefish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,21 +33,21 @@ set -x EDITOR 'nvim'
 set -x FZF_LEGACY_KEYBINDINGS 1
 
 # Bobthefish Theme
-# set -g theme_display_git no
-# set -g theme_display_git_untracked no
+set -g theme_display_git yes
+set -g theme_display_git_untracked yes
 set -g theme_display_git_ahead_verbose yes
-# set -g theme_git_worktree_support yes
-# set -g theme_display_vagrant yes
-# set -g theme_display_virtualenv no
-# set -g theme_display_ruby no
-# set -g theme_display_user yes
-# set -g theme_powerline_fonts no
+set -g theme_git_worktree_support no
+set -g theme_display_vagrant yes
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
+set -g theme_display_user no
+set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g theme_color_scheme solarized
-# set -g fish_prompt_pwd_dir_length 0
-# set -g theme_project_dir_length 1
-# set -g theme_newline_cursor yes
+set -g fish_prompt_pwd_dir_length 2
+set -g theme_project_dir_length 1
+set -g theme_newline_cursor no
 
 # Node Version Manager
 set -x NVM_DIR "$HOME/.nvm"
