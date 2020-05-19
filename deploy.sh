2010579e8efb747811a6bcc04a2d91f6461c8745
Shuhei Akutagawa <readonly@akutagawa.dev>
2020-05-19 16:56:42 +0900

    deploy.sh: Change command "[[" to "test"

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 cd `dirname $0`
 
 function deploy() {
-  if [[ -f $1 ]]; then
+  if test -f "$1"; then
     src=$(pwd)/$1
     dest=$HOME/$1
     ln -svnf $src $dest
   fi
 
-  if test -d $1; then
-    [[ -d $HOME/$1 ]] || mkdir -p $HOME/$1
+  if test -d "$1"; then
+    test -d $HOME/$1 || mkdir -p $HOME/$1
     for f in $(find $1 -maxdepth 1 ! -path $1); do
       deploy $f
     done
