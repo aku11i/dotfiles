a50d90e3bbfb2517fdc6b43edf94ba8236a58b07
Shuhei Akutagawa <readonly@akutagawa.dev>
2020-08-16 06:02:01 +0900

    fish: Change bobthefish colorscheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -87,7 +87,7 @@ set -g theme_display_user no
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
-set -g theme_color_scheme solarized
+set -g theme_color_scheme terminal
 set -g fish_prompt_pwd_dir_length 2
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor no
