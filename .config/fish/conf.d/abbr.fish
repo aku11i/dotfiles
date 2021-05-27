1774439029a06cde26541eb2c7a8a01732bd11d3
Shuhei Akutagawa <readonly@akutagawa.dev>
2021-05-27 19:00:38 +0900

    fish: Add tree alias

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -1,4 +1,3 @@
-# builtins
 abbr --add --global l 'ls -1a'
 abbr --add --global la 'ls -a'
 abbr --add --global l1 'ls -1a'
@@ -8,3 +7,5 @@ abbr --add --global cp 'cp -ir'
 abbr --add --global mv 'mv -i'
 abbr --add --global mkdir 'mkdir -p'
 abbr --add --global rm 'rm -i'
+
+abbr --add --global tree 'tree -N'
