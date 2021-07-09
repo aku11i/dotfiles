ebda34ef3f43e87ae8a036f7d6c9253101ca2e04
Shuhei Akutagawa <readonly@akutagawa.dev>
2021-07-09 15:28:10 +0900

    Fix removeBrokenSymlinks.sh

--- removeBrokenSymlinks.sh
+++ removeBrokenSymlinks.sh
@@ -3,13 +3,16 @@ cd `dirname $0`
 
 set -e
 
-brokenSymlinks=( $(find "$HOME" -maxdepth 1 -type l) )
+brokenSymlinks=("
+  $(find "$HOME"         -mindepth 1 -maxdepth 1 -xtype l)
+  $(find "$HOME/.config" -mindepth 1 -maxdepth 3 -xtype l)
+")
 
-dirs=("$HOME/.config" "$HOME/.bin")
-for dir in $dirs; do
-  test -d "$dir" || continue
-  brokenSymlinks=(${array[@]} $(find "$dir" -maxdepth 3 -xtype l) )
-done
+if test ${#array[@]} -eq "0"; then
+  exit 0
+fi
+
+echo "$brokenSymlinks"
 
 for link in $brokenSymlinks; do
   test -e "$link" && continue
