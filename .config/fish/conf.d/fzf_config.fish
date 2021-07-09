4286a4f3c25ad749fc23db5eb5c78b0fe395b303
Shuhei Akutagawa <readonly@akutagawa.dev>
2021-07-09 12:44:10 +0900

    fish: Change from PatrickF1/fzf.fish to jethrokuan/fzf

--- .config/fish/conf.d/fzf_config.fish
+++ .config/fish/conf.d/fzf_config.fish
@@ -1,1 +1,6 @@
-set -x FZF_DEFAULT_COMMAND "fd --type f --hidden"
+# fzf
+set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden'
+
+# fzf.fish
+set -x FZF_LEGACY_KEYBINDINGS '0'
+set -x FZF_FIND_FILE_COMMAND 'fd --type f --hidden'
