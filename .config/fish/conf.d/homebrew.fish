dd3aa485d41a378c429801c865ee1eff74f5edb3
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-09-22 12:52:33 +0900

    fish: GNU系のツールでパスを上書きしない

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -2,22 +2,6 @@
 if test (uname) = "Darwin"
   # Apple Silicon
   fish_add_path /opt/homebrew/bin
-
-  # GNU utils
-  fish_add_path /usr/local/opt/binutils/libexec/gnubin
-  fish_add_path /usr/local/opt/coreutils/libexec/gnubin
-  fish_add_path /usr/local/opt/diffutils/libexec/gnubin
-  fish_add_path /usr/local/opt/findutils/libexec/gnubin
-  fish_add_path /usr/local/opt/moreutils/libexec/gnubin
-  fish_add_path /usr/local/opt/gawk/libexec/gnubin
-  fish_add_path /usr/local/opt/gzip/libexec/gnubin
-  fish_add_path /usr/local/opt/gnu-tar/libexec/gnubin
-  fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
-  fish_add_path /usr/local/opt/gnu-time/libexec/gnubin
-  fish_add_path /usr/local/opt/gnu-getopt/libexec/gnubin
-
-  # Prioritize /usr/bin/uname over /usr/local/opt/coreutils/libexec/gnubin/uname
-  abbr uname '/usr/bin/uname'
 end
 
 
