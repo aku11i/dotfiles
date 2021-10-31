51ae291178e13ab98173f7a46a4d1635a54191d4
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2021-10-31 23:50:57 +0900

    chore: Fix brokenSymlinks.sh

--- removeBrokenSymlinks.sh
+++ removeBrokenSymlinks.sh
@@ -4,19 +4,19 @@ cd `dirname $0`
 set -e
 
 brokenSymlinks=("
-  $(find "$HOME"         -mindepth 1 -maxdepth 1 -xtype l)
-  $(find "$HOME/.config" -mindepth 1 -maxdepth 3 -xtype l)
+$(find "$HOME"         -mindepth 1 -maxdepth 1 -type l -exec test ! -e {} \; -print)
+$(find "$HOME/.config" -mindepth 1 -maxdepth 3 -type l -exec test ! -e {} \; -print)
 ")
 
-if test ${#array[@]} -eq "0"; then
+if test ${#brokenSymlinks[@]} -eq "0"; then
+  echo 'No broken symbolic links are found.'
   exit 0
 fi
 
+echo "Broken symbolic links are found:"
 echo "$brokenSymlinks"
 
 for link in $brokenSymlinks; do
-  test -e "$link" && continue
   rm -i "$link"
 done
-
 
