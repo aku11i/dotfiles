4472633fb212c7c198e27a19f9e9405038c81d14
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2023-06-18 22:25:18 +0900

    brew: Remove unused apps

--- caskfile.sh
+++ caskfile.sh
@@ -5,17 +5,14 @@ set -e
 brew install --cask \
   alacritty \
   alt-tab \
-  android-studio \
   appcleaner \
   bartender \
   docker \
   firefox \
   iterm2 \
-  jetbrains-toolbox \
   karabiner-elements \
   krisp \
   macvim \
-  microsoft-edge \
   raycast \
   the-unarchiver \
   vimr \
