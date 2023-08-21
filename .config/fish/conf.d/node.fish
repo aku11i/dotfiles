5545bd3978d4bd481150afb35f5c93e152063985
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2023-08-21 15:29:31 +0900

    feat(fish): Install pnpm completion

--- .config/fish/conf.d/node.fish
+++ .config/fish/conf.d/node.fish
@@ -18,3 +18,8 @@ abbr --add --global pn 'pnpm'
 abbr --add --global pni 'pnpm install'
 abbr --add --global pnr 'pnpm run'
 abbr --add --global pnx 'pnpm exec'
+
+# ↓ Added by `pnpm install-completion`
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
