85c506890f207b49ec03f436759bea6ea4015def
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2024-05-14 12:50:46 +0000

    fix: scripts/removeBrokenSymlinks.sh

--- scripts/removeBrokenSymlinks.sh
+++ scripts/removeBrokenSymlinks.sh
@@ -5,6 +5,7 @@ set -e
 
 brokenSymlinks=("
 $(find "$HOME"         -mindepth 1 -maxdepth 1 -type l -exec test ! -e {} \; -print)
+$(find "$HOME/.bin" -mindepth 1 -maxdepth 3 -type l -exec test ! -e {} \; -print)
 $(find "$HOME/.config" -mindepth 1 -maxdepth 3 -type l -exec test ! -e {} \; -print)
 ")
 
