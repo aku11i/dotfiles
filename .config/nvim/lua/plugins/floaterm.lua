a735b158142d914d0985a3b8726147d04dab34c0
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2024-07-07 00:37:40 +0000

    fix: enable FloatermToggle keymap in terminal mode

--- .config/nvim/lua/plugins/floaterm.lua
+++ .config/nvim/lua/plugins/floaterm.lua
@@ -6,6 +6,7 @@ return {
 	"voldikss/vim-floaterm",
 	config = function()
 		vim.keymap.set("n", "<C-t>", ":FloatermToggle<CR>", { noremap = true, silent = true })
+		vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:FloatermToggle<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>tt", ":FloatermNew<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>t]", ":FloatermNext<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>t[", ":FloatermPrev<CR>", { noremap = true, silent = true })
