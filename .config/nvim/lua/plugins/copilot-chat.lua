6974d5334915237ec24844ba70919b55dc6be595
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2024-12-29 04:04:07 +0900

    chore(nvim): update copilot-chat settings

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -27,18 +27,14 @@ return {
 
 		local prompts = chat.prompts()
 
-		prompts.Explain.prompt = "/COPILOT_EXPLAIN 選択中の箇所の内容を説明してください。"
-		prompts.Review.prompt = "/COPILOT_REVIEW 選択中のコードの内容をレビューしてください。"
+		prompts.Explain.prompt = "> /COPILOT_EXPLAIN\n\n選択中の箇所の内容を説明してください。"
+		prompts.Review.prompt =
+			"> /COPILOT_REVIEW\n\n選択中のコードの内容をレビューしてください。"
 		prompts.Fix.prompt =
-			"/COPILOT_GENERATE このコードには問題があります。 修正されるように書き直してください。"
+			"> /COPILOT_GENERATE\n\nこのコードには問題があります。 修正されるように書き直してください。"
 		prompts.Optimize.prompt =
-			"/COPILOT_GENERATE 選択中のコードをパフォーマンスと可読性が向上されるように書き直してください。"
-		prompts.Docs.prompt = "/COPILOT_GENERATE Please add documentation comment for the selection."
-		prompts.Tests.prompt = "/COPILOT_GENERATE Please generate tests for my code."
-		prompts.FixDiagnostic.prompt = "Please assist with the following diagnostic issue in file:"
-		prompts.Commit.prompt =
-			"Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit."
-		prompts.CommitStaged.prompt =
-			"Write commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit."
+			"> /COPILOT_GENERATE\n\n選択中のコードをパフォーマンスと可読性が向上されるように書き直してください。"
+		prompts.Docs.prompt = "> /COPILOT_GENERATE\n\nPlease add documentation comment for the selection."
+		prompts.Tests.prompt = "> /COPILOT_GENERATE\n\nPlease generate tests for my code."
 	end,
 }
