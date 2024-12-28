d03c570958e6bf78cf375c07a4cbfbc048a3626b
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2024-12-29 03:32:08 +0900

    chore: update alacritty

--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -5,63 +5,63 @@ function theme -d "Set terminal theme"
     case "catppuccin-frappe"
       set -Ux THEME "catppuccin-frappe"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/catppuccin_frappe.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/catppuccin_frappe.toml" ]' > ~/.config/alacritty/theme.toml
     case "catppuccin-latte"
       set -Ux THEME "catppuccin-latte"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/catppuccin_latte.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/catppuccin_latte.toml" ]' > ~/.config/alacritty/theme.toml
     case "catppuccin-macchiato"
       set -Ux THEME "catppuccin-macchiato"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/catppuccin_macchiato.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/catppuccin_macchiato.toml" ]' > ~/.config/alacritty/theme.toml
     case "catppuccin-mocha"
       set -Ux THEME "catppuccin-mocha"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/catppuccin_mocha.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/catppuccin_mocha.toml" ]' > ~/.config/alacritty/theme.toml
     case "everforest-light"
       set -Ux THEME "everforest-light"
       set -Ux DARK "false"
-      echo 'import = [ "~/.config/alacritty/themes/themes/everforest_light.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/everforest_light.toml" ]' > ~/.config/alacritty/theme.toml
     case "everforest-dark"
       set -Ux THEME "everforest-dark"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/everforest_dark.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/everforest_dark.toml" ]' > ~/.config/alacritty/theme.toml
     case "night-owl"
       set -Ux THEME "night-owl"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/night_owl.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/night_owl.toml" ]' > ~/.config/alacritty/theme.toml
     case "nord"
       set -Ux THEME "nord"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/nord.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/nord.toml" ]' > ~/.config/alacritty/theme.toml
     case "rose-pine"
       set -Ux THEME "rose-pine"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/rose-pine/dist/rose-pine.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/rose-pine/dist/rose-pine.toml" ]' > ~/.config/alacritty/theme.toml
     case "rose-pine-dawn"
       set -Ux THEME "rose-pine-dawn"
       set -Ux DARK "false"
-      echo 'import = [ "~/.config/alacritty/rose-pine/dist/rose-pine-dawn.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/rose-pine/dist/rose-pine-dawn.toml" ]' > ~/.config/alacritty/theme.toml
     case "rose-pine-moon"
       set -Ux THEME "rose-pine-moon"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/rose-pine/dist/rose-pine-moon.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/rose-pine/dist/rose-pine-moon.toml" ]' > ~/.config/alacritty/theme.toml
     case "solarized-dark"
       set -Ux THEME "solarized-dark"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/solarized_dark.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/solarized_dark.toml" ]' > ~/.config/alacritty/theme.toml
     case "solarized-light"
       set -Ux THEME "solarized-light"
       set -Ux DARK "false"
-      echo 'import = [ "~/.config/alacritty/themes/themes/solarized_light.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/solarized_light.toml" ]' > ~/.config/alacritty/theme.toml
     case "tokyo-night"
       set -Ux THEME "tokyo-night"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/tokyo-night.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/tokyo-night.toml" ]' > ~/.config/alacritty/theme.toml
     case "tokyo-night-storm"
       set -Ux THEME "tokyo-night-storm"
       set -Ux DARK "true"
-      echo 'import = [ "~/.config/alacritty/themes/themes/tokyo-night-storm.toml" ]' > ~/.config/alacritty/theme.toml
+      echo 'general.import = [ "~/.config/alacritty/themes/themes/tokyo-night-storm.toml" ]' > ~/.config/alacritty/theme.toml
   end
 end
 
